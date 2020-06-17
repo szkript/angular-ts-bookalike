55b707b3716d14c937645d00cf3cadd526a8ec7d
Szanyi Krisztián <szkript@gmail.com>
2020-06-17 22:50:02 +0200

    progress , canvas source solved

--- src/pdf-reader.service.ts
+++ src/pdf-reader.service.ts
@@ -9,16 +9,38 @@ export class PdfReaderService {
     pdfjsLib.GlobalWorkerOptions.workerSrc = 'assets/pdfjs/pdf.worker.js';
   }
 
-  public async readPdf(pdfUrl: string): Promise<Array<string>> {
+  public async readPdf(pdfUrl: string): Promise<Array<any>> {
     const pdf = await pdfjsLib.getDocument(pdfUrl).promise;
-    const countPromises = []; // collecting all page promises
-
-    for (let i = 1; i <= pdf.numPages; i++) {
-      const page = await pdf.getPage(i);
-      const textContent = await page.getTextContent();
-      countPromises.push(textContent.items.map((s) => s.str).join(''));
+    let pages = [];
+    for (let  i = 1; i < pdf.numPages; i++){
+      pdf.getPage(i).then(function (page) {
+        let scale = 1.5;
+        let viewport = page.getViewport({ scale: scale, });
+        let canvas = document.getElementById('the-canvas') as HTMLCanvasElement;
+        let context = canvas.getContext('2d');
+        canvas.height = viewport.height;
+        canvas.width = viewport.width;
+        let renderContext = {
+          canvasContext: context,
+          viewport: viewport
+        };
+        // page.render(renderContext);
+        pages.push(renderContext)
+      })
     }
-
-    return await Promise.all(countPromises);
+    return pages;
+    // const countPromises = []; // collecting all page promises
+    //
+    // for (let i = 1; i <= pdf.numPages; i++) {
+    //   const page = await pdf.getPage(i);
+    //   const textContent = await page.getTextContent();
+    //   countPromises.push(textContent.items.map((s) => s.str).join(''));
+    // }
+    //
+    // return await Promise.all(countPromises);
   }
+
+}
+function isCanvas(obj: HTMLCanvasElement | HTMLElement): obj is HTMLCanvasElement {
+  return obj.tagName === 'CANVAS';
 }
